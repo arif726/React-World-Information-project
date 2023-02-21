30b7561edf5f73100d5966d590b17c753e325e30
arif726 <akonarif3@gmail.com>
2023-02-21 22:51:24 +0600

    capital added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries/>
+      <Country/>
     </div>
   );
+}
+
+function Countries() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+
+    fetch('https://restcountries.com/v2/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+    
+  },[])
+  
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>Countries : {countries.length}</p>
+      {
+        countries.map(country=><Country name = {country.name} capital ={country.capital}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Capital : {props.capital}</p>
+    </div>
+  )
 }
 
 export default App;
